a141ccae96b2256ff6365d1ae0851be298225f43
sobradovic <jambalaya555@hotmail.com>
2020-05-01 17:03:36 +0200

    Removed duplicated getData() call

--- src/main/java/rs/raf/VoterUtil.java
+++ src/main/java/rs/raf/VoterUtil.java
@@ -2,14 +2,17 @@ package rs.raf;
 
 import org.apache.zookeeper.KeeperException;
 
+import java.util.Objects;
+
 class VoterUtil {
 
     // If the /leader node has been changed
     static void leaderChanged(ZooNode zooNode) throws KeeperException, InterruptedException {
         // Check if the leader data is "Start", if it is not, we assume the voting has been finished and get results
-        if (!voteIfLeaderStarted(zooNode)) {
+        String leaderData = voteIfLeaderStarted(zooNode);
+        if (Objects.nonNull(leaderData)) {
             // If the leader has the same value as the /vote node then we won
-            if (String.valueOf(zooNode.getMyVote()).equals(LeaderUtil.getLeaderData(zooNode))) {
+            if (String.valueOf(zooNode.getMyVote()).equals(leaderData)) {
                 System.out.println(zooNode.getId() + ": My vote (" + zooNode.getMyVote() + ") won!");
             }
             // Otherwise, we assume that we won, but this can also be checked by comparing the winning vote with other voting options
@@ -20,14 +23,16 @@ class VoterUtil {
     }
 
     // If the leader has "Start" data, that means we should vote
-    static boolean voteIfLeaderStarted(ZooNode zooNode) throws KeeperException, InterruptedException {
-        boolean leaderStarted = "Start".equals(LeaderUtil.getLeaderData(zooNode));
-        if (leaderStarted) {
+    // Returns null if we just voted, and leaderData if we didn't
+    static String voteIfLeaderStarted(ZooNode zooNode) throws KeeperException, InterruptedException {
+        String leaderData = LeaderUtil.getLeaderData(zooNode);
+        if ("Start".equals(leaderData)) {
             // We set our vote to be either 0 or 1
             zooNode.setMyVote(Util.binaryRandom());
             zooNode.getZk().setData(zooNode.getCurrentNode(), Integer.toHexString(zooNode.getMyVote()).getBytes(), 0);
+            return null;
         }
-        return leaderStarted;
+        return leaderData;
     }
 
 }
